b5177c41e0536bfc0f72f5749de9fb70d2c6aa2d
Keshav Bajwa <thecoder780@gmail.com>
2023-08-11 17:57:51 +0530

    Updated Code

--- controllers/flipkartController.js
+++ controllers/flipkartController.js
@@ -3,7 +3,7 @@ const cheerio = require('cheerio');
 const Product = require('../models/product');
 
 const BASE_URL = 'https://www.flipkart.com';
-const MOBILE_URL = `${BASE_URL}/mobiles`;
+const MOBILE_URL = `${BASE_URL}/search?q=mobile`;
 
 async function fetchMobileProducts() {
   try {
@@ -13,18 +13,31 @@ async function fetchMobileProducts() {
     const products = [];
 
     $('._1AtVbE').each((index, element) => {
-      const title = $(element).find('a').attr('title');
-      const price = $(element).find('div._30jeq3').text().trim();
+      const title = $(element).find('._4rR01T').text().trim();
+      let price = {}
+       price.Actualprice = $(element).find('div._27UcVY').text().trim();
+       price.discountPrice = $(element).find('div._1_WHN1').text().trim();
+       price.discountPercentage = $(element).find('div._3Ay6Sb').text().trim();
       const url = BASE_URL + $(element).find('a').attr('href');
       if (title || price || url) {
         products.push(new Product(title, price, url));
       }
     });
-
     return products;
   } catch (error) {
     throw error;
   }
 }
 
-module.exports = { fetchMobileProducts };
+async function flipkart (req, res) {
+  try {
+    const products = await fetchMobileProducts();
+    res.json(products);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred' });
+  }
+}
+
+
+module.exports = { fetchMobileProducts, flipkart };
